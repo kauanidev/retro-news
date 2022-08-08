dd0c3b371ec1848a16c6e46a59a1193a9587d662
Kauani <kauanihelora@gmail.com>
2022-08-08 02:06:19 -0300

    Correção - Imagem

--- src/components/LatestItem/index.jsx
+++ src/components/LatestItem/index.jsx
@@ -2,7 +2,7 @@ import { LatestItemContainer } from "./styles";
 
 export function LatestItem({ data }) {
   return (
-    <LatestItemContainer to="/detailedNews" state={data}>
+    <LatestItemContainer to="/detailedNews" state={data} image={data.image}>
       <p>{data.title}</p>
     </LatestItemContainer>
   );
